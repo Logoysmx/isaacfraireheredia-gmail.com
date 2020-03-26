5e47347aa20e7856bfede9f7e656b2160e4e94fd
Isaac <isaacfraireheredia@gmail.com>
2020-03-26 12:43:59 -0600

    Input/Output-examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'myPopup';
+  title = 'cualquier otra cosa';
+  obj = [{name: 'Isaac'}];
+  myString: string;
+
+  recibirData(e) {
+    this.myString = e;
+    console.log('Data que llega de event: ', e);
+  }
 }
